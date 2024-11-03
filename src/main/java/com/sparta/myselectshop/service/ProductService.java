17d8d7185e7afae63d7b5193608958023b2099a0
hyeririjeon <jeonhyeri5555@gmail.com>
2024-11-03 17:19:52 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -81,25 +81,37 @@ public class ProductService {
     }
 
     public void addFolder(Long productId, Long folderId, User user) {
-// 1) 상품을 조회합니다.
+    // 1) 상품을 조회합니다.
         Product product = productRepository.findById(productId).orElseThrow(() ->
                 new NullPointerException("해당 상품이 존재하지 않습니다.")
         );
-// 2) 폴더를 조회합니다.
+    // 2) 폴더를 조회합니다.
         Folder folder = folderRepository.findById(folderId).orElseThrow(
                 () -> new NullPointerException("해당 폴더가 존재하지 않습니다.")
         );
-// 3) 조회한 폴더와 상품이 모두 로그인한 회원의 소유인지 확인합니다.
+    // 3) 조회한 폴더와 상품이 모두 로그인한 회원의 소유인지 확인합니다.
         if (!product.getUser().getId().equals(user.getId())
                 || !folder.getUser().getId().equals(user.getId())) {
             throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다.");
         }
-// 중복확인
+    // 중복확인
         Optional<ProductFolder> overlapFolder = productFolderRepository.findByProductAndFolder(product, folder);
         if (overlapFolder.isPresent()) {
             throw new IllegalArgumentException("중복된 폴더입니다.");
         }
-// 4) 상품에 폴더를 추가합니다.
+    // 4) 상품에 폴더를 추가합니다.
         productFolderRepository.save(new ProductFolder(product, folder));
+    }
+
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+        // 페이징 처리
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        // 해당 폴더에 등록된 상품을 가져옵니다.
+        Page<Product> products = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+        Page<ProductResponseDto> responseDtoList = products.map(ProductResponseDto::new);
+        return responseDtoList;
+
     }
 }
