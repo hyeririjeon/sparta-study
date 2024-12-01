d74a6429570fda8c561597222af0c37677df398e
hyeririjeon <jeonhyeri5555@gmail.com>
2024-12-01 22:37:20 +0900

    init commit

---
+++ com.sparta.msa_exam/src/main/java/com/sparta/msa_exam/MsaExamApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.msa_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class MsaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaExamApplication.class, args);
+	}
+
+}
