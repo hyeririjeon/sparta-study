0bca033dca0f0bc70562a00d813e496e5f3f3366
hyeririjeon <jeonhyeri5555@gmail.com>
2024-12-03 17:51:51 +0900

    feat: 응답 header 에 server port 추가

--- com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/filter/CustomPostFilter.java
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/filter/CustomPostFilter.java
@@ -1,21 +1,29 @@
 package com.sparta.msa_exam.gateway.filter;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.DefaultResponse;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.logging.Logger;
 
 @Component
 public class CustomPostFilter implements GlobalFilter, Ordered{
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            ServerHttpResponse response = exchange.getResponse();
+            Object loadBalancerResponse = exchange.getAttribute("org.springframework.cloud.gateway.support.ServerWebExchangeUtils.gatewayLoadBalancerResponse");
+            if (loadBalancerResponse instanceof DefaultResponse) {
+                ServiceInstance serviceInstance = ((DefaultResponse) loadBalancerResponse).getServer();
+                if (serviceInstance != null) {
+                    String instancePort = String.valueOf(serviceInstance.getPort());
+                    exchange.getResponse().getHeaders().add("Server-Port", instancePort);
+                }
+            }
         }));
     }
 
